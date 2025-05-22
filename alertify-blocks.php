9cc3aaf866993c89df9b00e00b489e13f2ec7076
hmbashar <hmbashar@gmail.com>
2025-05-22 16:57:44 +0600

    init version

--- alertify-blocks.php
+++ alertify-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Alertify Blocks
  * Description: A collection of beautiful alert/notice blocks for the WordPress block editor.
- * Version: 1.0.0
+ * Version: 0.1.0
  * Author: MD.NESAR MRIDHA
  * Text Domain: alertify-blocks
  * License: GPL-2.0-or-later
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('ALERTIFY_BLOCKS_VERSION', '1.0.0');
+define('ALERTIFY_BLOCKS_VERSION', '0.1.0');
 define('ALERTIFY_BLOCKS_DIR', plugin_dir_path(__FILE__));
 define('ALERTIFY_BLOCKS_URL', plugin_dir_url(__FILE__));
 
